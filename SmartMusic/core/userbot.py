1409491849370e2bd21d5c5d27ecf48fc37cfe5f
Vishulover680 <vishal255g@gmail.com>
2025-10-22 10:04:14 +0530

    Replace 'tabahi_tabahi' with 'pallavisarkaar' in join_chat

--- SmartMusic/core/userbot.py
+++ SmartMusic/core/userbot.py
@@ -71,10 +71,10 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("tabahi_tabahi")
-                await self.one.join_chat("tabahi_tabahi")
-                await self.one.join_chat("tabahi_tabahi")
-                await self.one.join_chat("tabahi_tabahi")
+                await self.one.join_chat("pallavisarkaar")
+                await self.one.join_chat("pallavisarkaar")
+                await self.one.join_chat("pallavisarkaar")
+                await self.one.join_chat("pallavisarkaar")
 
             except:
                 pass
@@ -101,10 +101,10 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("tabahi_tabahi")
-                await self.two.join_chat("tabahi_tabahi")
-                await self.two.join_chat("tabahi_tabahi")
-                await self.two.join_chat("tabahi_tabahi")
+                await self.two.join_chat("pallavisarkaar")
+                await self.two.join_chat("pallavisarkaar")
+                await self.two.join_chat("pallavisarkaar")
+                await self.two.join_chat("pallavisarkaar")
             except:
                 pass
             assistants.append(2)
@@ -125,10 +125,10 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("tabahi_tabahi")
-                await self.three.join_chat("tabahi_tabahi")
-                await self.three.join_chat("tabahi_tabahi")
-                await self.three.join_chat("tabahi_tabahi")
+                await self.three.join_chat("pallavisarkaar")
+                await self.three.join_chat("pallavisarkaar")
+                await self.three.join_chat("pallavisarkaar")
+                await self.three.join_chat("pallavisarkaar")
             except:
                 pass
             assistants.append(3)
@@ -148,10 +148,10 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("tabahi_tabahi")
-                await self.four.join_chat("tabahi_tabahi")
-                await self.four.join_chat("tabahi_tabahi")
-                await self.four.join_chat("tabahi_tabahi")
+                await self.four.join_chat("pallavisarkaar")
+                await self.four.join_chat("pallavisarkaar")
+                await self.four.join_chat("pallavisarkaar")
+                await self.four.join_chat("pallavisarkaar")
             except:
                 pass
             assistants.append(4)
@@ -171,10 +171,10 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("tabahi_tabahi")
-                await self.five.join_chat("tabahi_tabahi")
-                await self.five.join_chat("tabahi_tabahi")
-                await self.five.join_chat("tabahi_tabahi")
+                await self.five.join_chat("pallavisarkaar")
+                await self.five.join_chat("pallavisarkaar")
+                await self.five.join_chat("pallavisarkaar")
+                await self.five.join_chat("pallavisarkaar")
             except:
                 pass
             assistants.append(5)
